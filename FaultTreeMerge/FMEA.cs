b2f793a4377e3397c6125ee151a9b18917e6d718
7lbarker <53226418+7lbarker@users.noreply.github.com>
2019-09-25 13:16:46 +0100

    Added FMEA object which stores a list of components

---
+++ FaultTreeMerge/FMEA.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace FaultTreeMerge
+{
+    class FMEA
+    {
+        public List<Component> Components = new List<Component>();
+
+        public FMEA(List<Component> pComponents)
+        {
+            Components = pComponents;
+        }
+
+        public FMEA()
+        {
+
+        }
+
+    }
+}
